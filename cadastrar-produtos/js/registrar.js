71e19235378a166564017b69eb48791a157bf6fd
GUILHERME  HENRIQUE PINHEIRO DA SILVA <0221432411021@fatectq.local>
2024-11-19 11:18:29 -0300

    EditCrudPT1

--- cadastrar-produtos/js/registrar.js
+++ cadastrar-produtos/js/registrar.js
@@ -91,6 +91,7 @@ async function listarInventario() {
     listar_cadastros.innerHTML = '';
     qI.forEach(doc => {
         if (doc.data().id != "placeholder") {
+            const idProduto = doc.id;
             const nomeProduto = doc.data().id;
             const valorProduto = doc.data().valor;
             const quantidadeProduto = doc.data().quantidade;
@@ -109,43 +110,74 @@ async function listarInventario() {
             li_quantidade.classList.add("fw-bold", "title-th", "text-center");
             li_quantidade.textContent = quantidadeProduto + "Kg";
 
+            let divFinal = document.createElement("div");
+            divFinal.classList.add("divFinal");
+
+            let divButtons = document.createElement("div");
+            divButtons.classList.add("divButtons");
+
             let li_valor_final = document.createElement("th");
-            li_valor_final.classList.add("fw-bold", "title-th", "text-center");
             li_valor_final.textContent = "R$" + valorFinal;
 
-            let li_remover = document.createElement("th");
-            li_remover.classList.add("btnRemover");
+            let btn_editar = document.createElement("button");
+            btn_editar.classList.add("btnEditar");
+
+            let svgEdit = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+            svgEdit.setAttribute("width", "100%");
+            svgEdit.setAttribute("height", "100%");
+            svgEdit.setAttribute("fill", "rgb(200, 200, 0)");
+            svgEdit.setAttribute("class", "bi bi-pencil");
+            svgEdit.setAttribute("viewBox", "0 0 16 16");
+
+            let pathEdit = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            pathEdit.setAttribute("d", "M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325");
+
+            svgEdit.appendChild(pathEdit);
+            btn_editar.appendChild(svgEdit);
 
             let btn_remover = document.createElement("button");
+            btn_remover.classList.add("btnRemover");
             let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-            svg.setAttribute("width", "10vh");
-            svg.setAttribute("height", "10vh");
+            svg.setAttribute("width", "100%");
+            svg.setAttribute("height", "100%");
             svg.setAttribute("fill", "red");
-            svg.setAttribute("class", "bi bi-x");
-            svg.setAttribute("viewBox", "0 0 32 32");
+            svg.setAttribute("class", "bi bi-trash3");
+            svg.setAttribute("viewBox", "0 0 16 16");
 
             let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-            path.setAttribute("d", "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708");
+            path.setAttribute("d", "M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5");
 
             svg.appendChild(path);
 
             btn_remover.addEventListener("click", function () {
+                removeProduto(idProduto);
                 tr.remove();
                 btn_remover.remove();
             });
 
             btn_remover.appendChild(svg);
-            li_remover.appendChild(btn_remover);
 
             tr.appendChild(li_produto);
             tr.appendChild(li_valor);
             tr.appendChild(li_quantidade);
-            tr.appendChild(li_valor_final);
-            tr.appendChild(li_remover);
+
+            divButtons.appendChild(btn_editar);
+            divButtons.appendChild(btn_remover);
+
+            divFinal.appendChild(li_valor_final);
+            divFinal.appendChild(divButtons);
+            
+            tr.appendChild(divFinal);
             listar_cadastros.appendChild(tr);
         }
     });
+}
+
+function removeProduto(produtoId) {
+    const docRef = doc(db, "Inventário/" + idInventario + "/inv/" + produtoId);
+    deleteDoc(docRef);
 }
 
 form.addEventListener("submit", function (e) {
